3fcbf0572aed94fc3127e4def251384b31fb1509
Mike Yang <yfractal@gmail.com>
2024-12-25 09:47:14 +0800

    feat: added scheduler func

--- ext/sdb_signal/src/lib.rs
+++ ext/sdb_signal/src/lib.rs
@@ -1,9 +1,14 @@
-use libc::{sigaction, sigemptyset, SA_RESTART, SA_SIGINFO, SIGPROF};
+use libc::{
+    pthread_create, pthread_t, raise, sigaction, sigemptyset, SA_RESTART, SA_SIGINFO, SIGPROF,
+};
 use magnus::{function, prelude::*, Error, Ruby};
 use std::mem::zeroed;
+use std::ptr;
+use std::thread::sleep;
+use std::time::Duration;
 
 extern "C" fn signal_handler(_: i32, _: *mut libc::siginfo_t, _: *mut libc::c_void) {
-    // Your signal handling logic here
+    println!("Signal received!");
 }
 
 fn setup_signal_handler() {
@@ -16,15 +21,26 @@ fn setup_signal_handler() {
     }
 }
 
-fn hello(subject: String) -> String {
-    format!("Hello from Rust, {subject}!")
+extern "C" fn scheduler_func(_: *mut libc::c_void) -> *mut libc::c_void {
+    loop {
+        sleep(Duration::from_millis(1000));
+        unsafe {
+            raise(SIGPROF);
+        }
+    }
+}
+
+fn start_scheduler() {
+    unsafe {
+        let mut thread: pthread_t = zeroed();
+        pthread_create(&mut thread, ptr::null(), scheduler_func, ptr::null_mut());
+    }
 }
 
 #[magnus::init]
 fn init(ruby: &Ruby) -> Result<(), Error> {
     let module = ruby.define_module("SdbSignal")?;
-    module.define_singleton_method("hello", function!(hello, 1))?;
     module.define_singleton_method("setup_signal_handler", function!(setup_signal_handler, 0))?;
-
+    module.define_singleton_method("start_scheduler", function!(start_scheduler, 0))?;
     Ok(())
 }
