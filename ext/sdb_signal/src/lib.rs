272a8b95f38806514c74c0e2f33bc06e753fba33
Mike Yang <yfractal@gmail.com>
2024-12-26 09:44:40 +0800

    feat: a basic scheduler and signal handler

--- ext/sdb_signal/src/lib.rs
+++ ext/sdb_signal/src/lib.rs
@@ -1,13 +1,23 @@
 use libc::{
-    pthread_create, pthread_t, raise, sigaction, sigemptyset, SA_RESTART, SA_SIGINFO, SIGPROF,
+    pthread_create, pthread_kill, pthread_self, pthread_t, sigaction, sigemptyset, SA_RESTART,
+    SA_SIGINFO, SIGPROF,
 };
 use magnus::{function, prelude::*, Error, Ruby};
+use rb_sys::{rb_profile_frames, VALUE};
 use std::mem::zeroed;
 use std::ptr;
 use std::thread::sleep;
 use std::time::Duration;
 
 extern "C" fn signal_handler(_: i32, _: *mut libc::siginfo_t, _: *mut libc::c_void) {
+    let mut buff: [VALUE; 10] = [0 as VALUE; 10];
+    let mut lines: [i32; 10] = [0; 10];
+    let frames = unsafe { rb_profile_frames(0, 10, buff.as_mut_ptr(), lines.as_mut_ptr()) };
+
+    println!("Signal received! Number of frames: {}", frames);
+    for i in 0..frames as usize {
+        println!("Frame {}: VALUE = {:?}, Line = {}", i, buff[i], lines[i]);
+    }
     println!("Signal received!");
 }
 
@@ -21,19 +31,29 @@ fn setup_signal_handler() {
     }
 }
 
-extern "C" fn scheduler_func(_: *mut libc::c_void) -> *mut libc::c_void {
+extern "C" fn scheduler_func(thread: *mut libc::c_void) -> *mut libc::c_void {
     loop {
         sleep(Duration::from_millis(1000));
         unsafe {
-            raise(SIGPROF);
+            pthread_kill(thread as pthread_t, SIGPROF);
         }
     }
+}
+
+fn get_current_thread_id() -> pthread_t {
+    unsafe { pthread_self() }
 }
 
 fn start_scheduler() {
     unsafe {
         let mut thread: pthread_t = zeroed();
-        pthread_create(&mut thread, ptr::null(), scheduler_func, ptr::null_mut());
+        let current_thread = get_current_thread_id();
+        pthread_create(
+            &mut thread,
+            ptr::null(),
+            scheduler_func,
+            current_thread as *mut libc::c_void,
+        );
     }
 }
 
--- scripts/measure_cpu_usage.rb
+++ scripts/measure_cpu_usage.rb
@@ -1,3 +1,5 @@
 require 'sdb_signal'
 
+SdbSignal.setup_signal_handler
+SdbSignal.start_scheduler
 SdbSignal.sleep_with_gvl
